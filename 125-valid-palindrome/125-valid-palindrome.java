2e5ef193521c3eb3d6c559f7f8474d623021ce6e
Vibhore Maheshwari <vibhore1997@gmail.com>
2022-01-14 13:54:26 -0500

    Time: 8 ms (41.15%), Space: 41.1 MB (16.55%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String lower = s.toLowerCase();
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i<lower.length(); i++) {
+            if (lower.charAt(i) >= 'a' && lower.charAt(i) <= 'z') {
+                sb.append(lower.charAt(i));
+            } else if (lower.charAt(i) >= '0' && lower.charAt(i) <= '9') {
+                sb.append(lower.charAt(i));
+            }
+        }
+        
+        for (int i=0, j = sb.length() - 1; i<j; i++, j--) {
+            if (sb.charAt(i) != sb.charAt(j)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
