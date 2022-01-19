27c5e7ce1ae600395e00bc2ecaa568a3fec73e5e
Vibhore Maheshwari <vibhore1997@gmail.com>
2022-01-18 22:25:34 -0500

    Time: 4 ms (82.20%), Space: 39.3 MB (55.44%) - LeetHub

---
+++ 71-simplify-path/71-simplify-path.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String simplifyPath(String path) {
+        String[] comp = path.split("/");
+        Stack<String> stack = new Stack<>();
+        
+        
+        for (String ele : comp) {
+            if (ele.equals(".") || ele.equals("")) {
+                continue;
+            } else if (ele.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(ele);
+            }
+        }
+        
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (String s : stack) {
+            sb.append("/");
+            sb.append(s);
+        }
+        
+        if (sb.length() == 0) {
+            sb.append("/");
+        }
+        return sb.toString();
+    }
+}
