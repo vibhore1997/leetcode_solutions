d94a9303ea149f9734c3099801a95854807513d6
Vibhore Maheshwari <vibhore1997@gmail.com>
2022-01-21 02:50:45 -0500

    Time: 60 ms (25.83%), Space: 46 MB (14.60%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -1,3 +1,38 @@
+// class Solution {
+//     public List<Integer> findAnagrams(String s, String p) {
+//         List<Integer> result = new ArrayList<>();
+//         int i = 0, j = 0;
+//         int k = p.length();
+//         Map<Character, Integer> hmap = new HashMap<>();
+//         for (char c : p.toCharArray()) {
+//             hmap.put(c, hmap.getOrDefault(c, 0) + 1);
+//         }
+//         Map<Character, Integer> map2 = new HashMap<>();
+//         while (j < s.length()) {
+//             if (j < k-1) {
+//                 map2.put(s.charAt(j), map2.getOrDefault(s.charAt(j), 0) + 1);
+//                 j++;
+//             } else {
+//                 map2.put(s.charAt(j), map2.getOrDefault(s.charAt(j), 0) + 1);
+                
+//                 if (hmap.equals(map2)) {
+//                     result.add(i);
+//                 }
+                
+//                 if (map2.get(s.charAt(i)) == 1) {
+//                     map2.remove(s.charAt(i));
+//                 } else {
+//                     map2.put(s.charAt(i), map2.get(s.charAt(i)) - 1);
+//                 }
+                
+//                 i++;
+//                 j++;
+//             }
+//         }
+//         return result;
+//     }
+// }
+
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> result = new ArrayList<>();
@@ -7,22 +42,37 @@ class Solution {
         for (char c : p.toCharArray()) {
             hmap.put(c, hmap.getOrDefault(c, 0) + 1);
         }
-        Map<Character, Integer> map2 = new HashMap<>();
+       int count = hmap.size();
         while (j < s.length()) {
             if (j < k-1) {
-                map2.put(s.charAt(j), map2.getOrDefault(s.charAt(j), 0) + 1);
+                char c = s.charAt(j);
+                if (hmap.containsKey(c)) {
+                    hmap.put(c, hmap.get(c) - 1);
+                    if (hmap.get(c) == 0) {
+                        count--;
+                    }
+                }
                 j++;
-            } else {
-                map2.put(s.charAt(j), map2.getOrDefault(s.charAt(j), 0) + 1);
                 
-                if (hmap.equals(map2)) {
+            } else {
+                char c = s.charAt(j);
+                if (hmap.containsKey(c)) {
+                    hmap.put(c, hmap.get(c) - 1);
+                    if (hmap.get(c) == 0) {
+                        count--;
+                    }
+                }
+                
+                if (count == 0) {
                     result.add(i);
                 }
                 
-                if (map2.get(s.charAt(i)) == 1) {
-                    map2.remove(s.charAt(i));
-                } else {
-                    map2.put(s.charAt(i), map2.get(s.charAt(i)) - 1);
+                if (hmap.containsKey(s.charAt(i))) {
+                    int value = hmap.get(s.charAt(i));
+                    hmap.put(s.charAt(i), value + 1);
+                    if (value + 1 == 1) {
+                        count++;
+                    }
                 }
                 
                 i++;
