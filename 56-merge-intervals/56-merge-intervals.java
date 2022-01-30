36f0fa2523099d73b655894e609f979c357a158f
Vibhore Maheshwari <vibhore1997@gmail.com>
2022-01-29 22:12:41 -0500

    Time: 19 ms (19.83%), Space: 55.1 MB (5.36%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,3 +1,45 @@
+// class Solution {
+//     public int[][] merge(int[][] intervals) {
+//         if (intervals.length == 1) {
+//             return intervals;
+//         }
+        
+//         Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
+        
+//         Stack<int[]> stack = new Stack<>();
+//         stack.push(intervals[0]);
+        
+//         for(int i = 1; i<intervals.length; i++) {
+//             int[] top = stack.pop();
+//                 if (top[1] >= intervals[i][0]) {
+//                     if (top[1] >= intervals[i][1]) {
+//                         stack.push(top);
+//                     } else {
+//                         stack.push(new int[]{top[0], intervals[i][1]});
+//                     }
+//                 } else {
+//                     stack.push(top);
+//                     stack.push(new int[]{intervals[i][0], intervals[i][1]});
+//                 }
+//         }
+        
+//         int res[][] = new int[stack.size()][2];
+//         int k = 0;
+        
+//         while (stack.size() != 0) {
+//             res[k++] = stack.pop();
+//         }
+        
+//         for (int i=0, j = res.length - 1; i < j; i++, j--) {
+//             int[] temp = res[i];
+//             res[i] = res[j];
+//             res[j] = temp;
+//         }
+        
+//         return res;
+//     }
+// }
+
 class Solution {
     public int[][] merge(int[][] intervals) {
         if (intervals.length == 1) {
@@ -6,34 +48,21 @@ class Solution {
         
         Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
         
-        Stack<int[]> stack = new Stack<>();
-        stack.push(intervals[0]);
+        List<int[]> list = new ArrayList<>();
+        list.add(intervals[0]);
         
         for(int i = 1; i<intervals.length; i++) {
-            int[] top = stack.pop();
-                if (top[1] >= intervals[i][0]) {
-                    if (top[1] >= intervals[i][1]) {
-                        stack.push(top);
-                    } else {
-                        stack.push(new int[]{top[0], intervals[i][1]});
-                    }
-                } else {
-                    stack.push(top);
-                    stack.push(new int[]{intervals[i][0], intervals[i][1]});
-                }
+            if (list.get(list.size() - 1)[1] < intervals[i][0]) {
+                list.add(intervals[i]);
+            } else {
+                list.get(list.size() - 1)[1] = Math.max(list.get(list.size() - 1)[1], intervals[i][1]);
+            }
         }
         
-        int res[][] = new int[stack.size()][2];
-        int k = 0;
+        int res[][] = new int[list.size()][2];
         
-        while (stack.size() != 0) {
-            res[k++] = stack.pop();
-        }
-        
-        for (int i=0, j = res.length - 1; i < j; i++, j--) {
-            int[] temp = res[i];
-            res[i] = res[j];
-            res[j] = temp;
+        for(int k = 0; k<res.length; k++) {
+            res[k] = list.get(k);
         }
         
         return res;
