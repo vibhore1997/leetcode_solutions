db58676a3f4540a740f59ced17ae85221a5bd08b
Vibhore Maheshwari <vibhore1997@gmail.com>
2022-02-03 18:26:55 -0500

    Time: 2 ms (87.73%), Space: 45.6 MB (9.87%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -39,25 +39,48 @@
 //     }
 // }
 
+// class Solution { // DP
+    
+//     public int minPathSum(int[][] grid) {
+//         int dp[][] = new int[grid.length][grid[0].length];
+        
+//         for (int i=0; i<grid.length; i++) {
+//             for (int j=0; j<grid[0].length; j++) {
+//                 if (i == 0 && j == 0) {
+//                     dp[i][j] = grid[i][j];
+//                 } else if (i == 0) {
+//                     dp[i][j] = grid[i][j] + dp[i][j-1];
+//                 } else if (j == 0) {
+//                     dp[i][j] = grid[i][j] + dp[i-1][j];
+//                 } else {
+//                     dp[i][j] = grid[i][j] + Math.min(dp[i-1][j], dp[i][j-1]);
+//                 }
+//             }
+//         }
+//         return dp[dp.length-1][dp[0].length-1];
+        
+//     }
+// }
+
 class Solution {
     
     public int minPathSum(int[][] grid) {
-        int dp[][] = new int[grid.length][grid[0].length];
+        // int dp[][] = new int[grid.length][grid[0].length];
         
         for (int i=0; i<grid.length; i++) {
             for (int j=0; j<grid[0].length; j++) {
                 if (i == 0 && j == 0) {
-                    dp[i][j] = grid[i][j];
+                   continue;
                 } else if (i == 0) {
-                    dp[i][j] = grid[i][j] + dp[i][j-1];
+                    grid[i][j] = grid[i][j] + grid[i][j-1];
                 } else if (j == 0) {
-                    dp[i][j] = grid[i][j] + dp[i-1][j];
+                    grid[i][j] = grid[i][j] + grid[i-1][j];
                 } else {
-                    dp[i][j] = grid[i][j] + Math.min(dp[i-1][j], dp[i][j-1]);
+                    grid[i][j] = grid[i][j] + Math.min(grid[i-1][j], grid[i][j-1]);
                 }
             }
         }
-        return dp[dp.length-1][dp[0].length-1];
+        return grid[grid.length-1][grid[0].length-1];
         
     }
 }
