536cb0d77faef749e3caee46aba6e19b87a3ad23
Vibhore Maheshwari <vibhore1997@gmail.com>
2022-02-04 17:14:37 -0500

    Time: 1 ms (100.00%), Space: 42.8 MB (24.67%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<>();
-        // Arrays.sort(candidates);
+        Arrays.sort(candidates);
         
         backtrack(candidates, res, target, 0, new ArrayList<>(), 0);
         return res;
@@ -17,6 +17,9 @@ class Solution {
         
         for (int i=pos; i<arr.length; i++) {
             sum += arr[i];
+            if (sum > target) {
+                break;
+            }
             list.add(arr[i]);
             
             backtrack(arr, res, target, sum, list, i);
