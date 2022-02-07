8f630438bead9831c096870df3f8e16bd63a089b
Vibhore Maheshwari <vibhore1997@gmail.com>
2022-02-07 14:12:19 -0500

    Time: 40 ms (13.72%), Space: 55.8 MB (27.66%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,19 +1,42 @@
+// class Solution {
+//     public int kthSmallest(int[][] matrix, int k) {
+//         Queue<Integer> heap = new PriorityQueue<>((a,b) -> b - a); Max heap, brute
+        
+//         for(int i=0; i<matrix.length; i++) {
+//             for(int j=0; j<matrix[0].length; j++) {
+//                 if(heap.size() < k) {
+//                     heap.offer(matrix[i][j]);
+//                 } else {
+//                     if (matrix[i][j] < heap.peek()) {
+//                         heap.poll();
+//                         heap.offer(matrix[i][j]);
+//                     }
+//                 }
+//             }
+//         }
+//         return heap.peek();
+//     }
+// }
+
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        Queue<Integer> heap = new PriorityQueue<>((a,b) -> b - a);
+        Queue<int[]> heap = new PriorityQueue<>((a,b) -> a[0] - b[0]); // Min heap, like K sorted lists
         
-        for(int i=0; i<matrix.length; i++) {
-            for(int j=0; j<matrix[0].length; j++) {
-                if(heap.size() < k) {
-                    heap.offer(matrix[i][j]);
-                } else {
-                    if (matrix[i][j] < heap.peek()) {
-                        heap.poll();
-                        heap.offer(matrix[i][j]);
-                    }
-                }
-            }
+        for(int i=0; i<Math.min(matrix.length, k); i++) {
+            heap.offer(new int[]{matrix[i][0], i, 0});
         }
-        return heap.peek();
+        int ans = 0;
+        
+        while(k > 0) {
+            int[] minEle = heap.poll();
+            ans = minEle[0];
+            if (minEle[2] + 1 < matrix[0].length) {
+                int r = minEle[1];
+                int c = minEle[2] + 1;
+                heap.offer(new int[]{matrix[r][c], r, c});
+            }
+            k--;
+        }
+        return ans;
     }
 }
