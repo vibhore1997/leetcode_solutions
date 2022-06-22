e4431a09db729a13d4c2fab7e7ac8e5c15f95362
Vibhore Maheshwari <vibhore1997@gmail.com>
2022-06-21 20:06:24 -0400

    Time: 30 ms (38.69%), Space: 54.4 MB (32.30%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.java
@@ -1,35 +1,32 @@
 class Solution {
     public String frequencySort(String s) {
-        
-    if (s == null || s.isEmpty()) return s;
-    
-    // Create a sorted Array of chars.
-    char[] chars = s.toCharArray();
-    Arrays.sort(chars);
-    
-    // Convert identical chars into single Strings.
-    List<String> charStrings = new ArrayList<String>();
-    StringBuilder currentString = new StringBuilder();
-    currentString.append(chars[0]);
-    for (int i = 1; i < chars.length; i++) {
-        if (chars[i] != chars[i - 1]) {
-            charStrings.add(currentString.toString());
-            currentString = new StringBuilder();
+        if (s == null){
+            return null;
         }
-        currentString.append(chars[i]);
-    }
-    charStrings.add(currentString.toString());
         
-    // Our comparator is (a, b) -> b.length() - a.length().
-    // If a is longer than b, then a negative number will be returned
-    // telling the sort algorithm to place a first. Otherwise, a positive
-    // number will be returned, telling it to place a second.
-    // This results in a longest to shortest sorted list of the strings.
-    Collections.sort(charStrings, (a, b) -> b.length() - a.length());
-    
-    // Use StringBuilder to build the String to return.
-    StringBuilder sb = new StringBuilder();
-    for (String str : charStrings) sb.append(str);
-    return sb.toString();
-}
+        Map<Character, Integer> hmap = new HashMap<>();
+        
+        for (char c : s.toCharArray()) {
+            hmap.put(c, hmap.getOrDefault(c, 0) + 1);
+        }
+        
+        List<Map.Entry<Character, Integer>> list = new ArrayList<>(hmap.entrySet());
+        
+        Collections.sort(list, (a,b) -> {
+            return b.getValue() - a.getValue();
+        });
+        
+        StringBuilder res = new StringBuilder();
+        
+        for (Map.Entry<Character, Integer> entry : list) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i<entry.getValue(); i++) {
+                sb.append(entry.getKey());
+            }
+            res.append(sb.toString());
+        }
+        
+        return res.toString();
+        
+  }
 }
