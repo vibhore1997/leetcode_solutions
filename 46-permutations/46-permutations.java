508d630e9f3d5b06693a7c4c290b8e4e704b7a75
Vibhore Maheshwari <vibhore1997@gmail.com>
2022-08-02 16:35:18 -0400

    Time: 2 ms (77.49%), Space: 44.7 MB (48.21%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,23 +1,22 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        boolean used[] = new boolean[nums.length];
-        helper(res, new ArrayList<>(), used, nums);
-        return res;
+        boolean[] used = new boolean[nums.length];
+        List<List<Integer>> ans = new ArrayList<>();
+        track(nums, ans, used, new ArrayList<Integer>());
+        return ans;
     }
     
-    void helper(List<List<Integer>> res, List<Integer> list, boolean[] used, int[] nums) {
-        if (nums.length == list.size()) {
-            res.add(new ArrayList<>(list));
+    private void track(int[] nums,  List<List<Integer>> ans, boolean[] used, List<Integer> list) {
+        if (list.size() == nums.length) {
+            ans.add(new ArrayList<>(list));
             return;
         }
         
         for(int i=0; i<nums.length; i++) {
-            if (used[i] == false) {
+            if (!used[i]) {
                 used[i] = true;
                 list.add(nums[i]);
-                helper(res, list, used, nums);
-                
+                track(nums, ans, used, list);
                 used[i] = false;
                 list.remove(list.size() - 1);
             }
