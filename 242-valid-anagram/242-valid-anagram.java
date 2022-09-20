8efc5d56cf79ca3acae8dbe80da8b2fa8eca99c0
Vibhore Maheshwari <vibhore1997@gmail.com>
2022-09-20 01:04:55 -0400

    Time: 3 ms (95.20%), Space: 42.2 MB (96.15%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int arr[] = new int[26];
+        
+        if (s.length() != t.length())
+            return false;
+        
+        for(int i=0 ; i < s.length(); i++) {
+            arr[s.charAt(i) - 'a']++;
+            arr[t.charAt(i) - 'a']--;
+        }
+        
+        for(int i=0; i< arr.length; i++) {
+            if (arr[i] != 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
