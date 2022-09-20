c6e4f69be098f8c41e2eb7dc864e7886c96ae30c
Vibhore Maheshwari <vibhore1997@gmail.com>
2022-09-20 01:13:41 -0400

    Time: 28 ms (15.78%), Space: 46.6 MB (15.66%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,21 +1,50 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int arr[] = new int[26];
+//         int arr[] = new int[26];
+        
+//         if (s.length() != t.length())
+//             return false;
+        
+//         for(int i=0 ; i < s.length(); i++) {
+//             arr[s.charAt(i) - 'a']++;
+//             arr[t.charAt(i) - 'a']--;
+//         }
+        
+//         for(int i=0; i< arr.length; i++) {
+//             if (arr[i] != 0) {
+//                 return false;
+//             }
+//         }
+        
+//         return true;
+        
         
         if (s.length() != t.length())
             return false;
         
-        for(int i=0 ; i < s.length(); i++) {
-            arr[s.charAt(i) - 'a']++;
-            arr[t.charAt(i) - 'a']--;
+        Map<Character, Integer> hmap = new HashMap<>();
+        
+        for (int i=0; i<s.length(); i++) {
+            hmap.put(s.charAt(i), hmap.getOrDefault(s.charAt(i), 0) + 1);
         }
         
-        for(int i=0; i< arr.length; i++) {
-            if (arr[i] != 0) {
+        for (int i=0; i<t.length(); i++) {
+            if (hmap.containsKey(t.charAt(i))) {
+                int val = hmap.get(t.charAt(i));
+                val--;
+                if (val == 0) {
+                    hmap.remove(t.charAt(i));
+                } else {
+                    hmap.put(t.charAt(i), val);
+                }
+            } else {
                 return false;
             }
         }
         
-        return true;
+        if (hmap.size() == 0) {
+            return true;
+        }
+        return false;
     }
 }
