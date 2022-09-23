4ce8a1f8731cd945b72999452fcd74cc5a85b069
Vibhore Maheshwari <vibhore1997@gmail.com>
2022-09-23 01:11:07 -0400

    Time: 10 ms (50.48%), Space: 44 MB (50.16%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -4,19 +4,17 @@ class Solution {
         
         StringBuilder sb = new StringBuilder();
         
-        for (int i = 0; i<lower.length(); i++) {
-            if (lower.charAt(i) >= 'a' && lower.charAt(i) <= 'z') {
+        for (int i=0; i<lower.length(); i++) {
+            if ((lower.charAt(i) >= 'a' && lower.charAt(i) <= 'z') || (lower.charAt(i) >= '0' && lower.charAt(i) <= '9')) {
                 sb.append(lower.charAt(i));
-            } else if (lower.charAt(i) >= '0' && lower.charAt(i) <= '9') {
-                sb.append(lower.charAt(i));
-            }
         }
+    }
         
-        for (int i=0, j = sb.length() - 1; i<j; i++, j--) {
+        for (int i=0, j = sb.length() - 1; i < j; i++, j--) {
             if (sb.charAt(i) != sb.charAt(j)) {
                 return false;
             }
         }
-        return true;
-    }
+ return true;       
+}
 }
